f8b388a3f704471e72db441618f329a9bb43ee3d
Rexios <rexios@rexios.dev>
2022-11-03 23:02:34 -0400

    ...

--- lib/firebase_ui_oauth_oidc.dart
+++ lib/firebase_ui_oauth_oidc.dart
@@ -1,4 +1,7 @@
 library firebase_ui_oauth_oidc;
 
+export 'package:firebase_ui_oauth/firebase_ui_oauth.dart'
+    show ThemedColor, ThemedIconSrc;
+
 export 'src/provider.dart' show OidcProvider;
 export 'src/theme.dart' show OidcProviderButtonStyle;
